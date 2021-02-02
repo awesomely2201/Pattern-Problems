8df9b893cdb0d287bfe433e6db5afe2b6f53992a
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-02 11:38:47 +0530

    Downward Triangle Star Pattern
    * * * * * 
    * * * * 
    * * * 
    * * 
    *

---
+++ downward.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+    Scanner sc = new Scanner(System.in);
+ 
+    System.out.println("Enter the number of rows: "); //takes input from user
+ 
+    int rows = sc.nextInt();
+ 
+    for (int i= rows-1; i>=0 ; i--)
+    {
+    for (int j=0; j<=i; j++)
+    {
+    System.out.print("*" + " ");
+    }
+    System.out.println();
+    }
+    sc.close();
+    }
+    }
