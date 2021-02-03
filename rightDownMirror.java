9157e44f639e1881d77b0eef0790cf8c31f4a421
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-03 11:10:19 +0530

    Right Down Mirror Star Pattern
    *****
     ****
      ***
       **
        *

---
+++ rightDownMirror.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+ 
+    public static void main(String[] args)
+    {
+    Scanner sc = new Scanner(System.in); // takes input
+    System.out.println("Enter number of rows: ");
+    int rows = sc.nextInt();
+    for (int i= rows; i>= 1; i--)
+    {
+    for (int j=rows; j>i;j--)
+    {
+    System.out.print(" ");
+    }
+    for (int k=1;k<=i;k++)
+    {
+    System.out.print("*");
+    }
+    System.out.println("");
+    }
+    }
+    }
