f9bee467aec06f5b066547afbda2addb4065033e
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-06 23:47:42 +0530

    Zero/Ones Number Pattern
    1
    10
    101
    1010
    10101

---
+++ zeroOne.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ 
+public class Main
+{            
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+          
+        System.out.println("Enter the number of rows: ");
+          
+        int rows = sc.nextInt();         
+        for (int i = 1; i <= rows; i++) 
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if(j%2 == 0)
+                {
+                    System.out.print(0);
+                }
+                else
+                {
+                    System.out.print(1);
+                }
+            }
+              
+            System.out.println();
+        }
+          
+        sc.close();
+    }
+}
