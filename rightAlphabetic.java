ff8c43aa868f30af635613e1a0559592c432ca05
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-06 23:50:19 +0530

    Right Alphabetic Pattern
    A 
    A B 
    A B C 
    A B C D 
    A B C D E 
    A B C D E F

---
+++ rightAlphabetic.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Main
+{            
+    public static void main(String[] args)
+    {
+        int alphabet = 65;
+                for (int i = 0; i <= 5; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
